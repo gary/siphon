fdd922582d248fb18f83556c4483ccd700231d47
Gary Iams <gary@users.noreply.github.com>
2026-03-16 15:29:16 -0400

    Expose endpoint for search

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :tasks do
+    resources :searches, only: :index
+  end
+
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
